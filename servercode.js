3f38cef784506116ab1e34428f7a72b8baa61512
makepaddev <info@makepad.io>
2017-12-06 02:28:08 +0100

    proxy

--- servercode.js
+++ servercode.js
@@ -1,10 +1,12 @@
 console.log("Hello on server!")
+
 var https = require('https')
+var Url = require('url')
 
 exports.onRequest = function(req, res){
+	var parsed = Url.parse(req.url)
 	console.log("Browser is requesting:", req.url)
-
-	if(req.url === '/urltoget'){
+	if(parsed.pathname === '/urltoget'){
 		res.writeHead(200,{'Content-Type':'text/text'})
 		res.end('get data from server')
 		// lets do a forward request (play proxy)
